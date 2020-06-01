3bbc683cae824e5e0be54e244a175d66ab55844e
dennisbuckel <dennis.buck@web.de>
2020-06-01 18:09:55 +0200

    added buttons to switch between activities added changeBackground()

--- app/src/main/java/com/example/stargame/GameActivity.java
+++ app/src/main/java/com/example/stargame/GameActivity.java
@@ -1,7 +1,11 @@
 package com.example.stargame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,8 +16,21 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gamelayout);
-    }
+        timer.run();
 
+
+        Button exit = findViewById(R.id.exit);
+        exit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+    
+   
+    
 
     private Handler mHandler = new Handler();
     private int zaehler = 0;
@@ -25,10 +42,21 @@ public class GameActivity extends AppCompatActivity {
         @Override
         public void run() {
             Toast.makeText(GameActivity.this, "Zähler " + zaehler, Toast.LENGTH_SHORT).show();
-            mHandler.postDelayed(timer, 2000);
+            changeBackground();
+            mHandler.postDelayed(timer, 10000);
             zaehler += 1;
         }
     };
+
+    LinearLayout linear = (LinearLayout) findViewById(R.id.linear);
+    
+    public void changeBackground(){
+        if(zaehler %2==0  ){
+          linear.setBackgroundResource(R.drawable.hintergrund1);  
+        }else{
+            linear.setBackgroundResource(R.drawable.hintergrund2); 
+        }
+    }
 
 
 }
--- app/src/main/java/com/example/stargame/MainActivity.java
+++ app/src/main/java/com/example/stargame/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.stargame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainlayout);
+
+        Button start = findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               launchActivity();
+            }
+        });
+
+
+
     }
+
+    public void launchActivity(){
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
+
+
+
 }
+
+
+
+
